aa9cdfd270b8a83b351d9eacf1dffd7a1de510b4
int2001 <joerg@dj7nt.de>
2023-07-22 12:54:51 +0200

    Early Dev

--- testclient.pl
+++ testclient.pl
@@ -8,7 +8,5 @@ use Gearman::Client;
 $|=1;
 
 my $client  = Gearman::Client->new(job_servers => ['127.0.0.1:4730']);
-my $x=$client->do_task("dxcc", "FK8GM");
-print Dumper $x;
-$x=$client->do_task("dxcc", "ON4ROL");
+my $x=$client->do_task("dxcc_test", "FK8GM");
 print Dumper $x;
